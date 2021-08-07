56bc1876959bb5b3c4b8ccba5241f194b119b72b
arek <arekrucki@hotmail.co.uk>
2021-08-07 10:28:06 +0100

    browser tester

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,9 @@
 import './App.css';
+import MainContainer from './containers/MainContainer';
 
 function App() {
   return (
-    <h1>CONTAINER</h1>
+    <MainContainer />
   );
 }
 
---
+++ client/src/containers/MainContainer.js
@@ -0,0 +1,20 @@
+import React,{ useState, useEffect } from 'react';
+
+
+
+const MainContainer = () => {
+    // const [companies ,setCompanies] = useState([])
+    // const [player, setPlayer] = useState("")
+    // const [totalCapital, steTotalCapital] = useState(100000)
+
+    // useEffect(() => {
+    //     getCompanies();
+    // })
+
+    return (
+    <h2>Hello World</h2>
+    )
+}
+
+export default MainContainer;
+
