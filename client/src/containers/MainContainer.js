66ff14cf554282957a3418a2ad47c8f8968e2e7f
Nathan <nathan.rendall@gmail.com>
2021-08-09 10:35:02 +0100

    fetch request to api added, with useEffect sand useState

--- client/src/containers/MainContainer.js
+++ client/src/containers/MainContainer.js
@@ -1,15 +1,23 @@
 import React,{ useState, useEffect } from 'react';
 
-
-
 const MainContainer = () => {
-    // const [companies ,setCompanies] = useState([])
+
+    const [companies , setCompanies] = useState([])
+
     // const [player, setPlayer] = useState("")
     // const [totalCapital, steTotalCapital] = useState(100000)
 
-    // useEffect(() => {
-    //     getCompanies();
-    // })
+    useEffect(() => {
+        getCompanies();
+    }, []);
+
+    const getCompanies = () => {
+        fetch('https://www.styvio.com/api/kxin', 'https://www.styvio.com/api/aapl')
+        .then(res => res.json())
+        .then(companiesData => setCompanies(companiesData));
+    };
+
+
 
     return (
     <h2>Hello World</h2>
