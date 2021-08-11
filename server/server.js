c00a0f0771af2cff62e8357fbec5f181f45d0264
Nathan <nathan.rendall@gmail.com>
2021-08-11 11:06:15 +0100

    new seeds file created for playerInfo, get and put routes added into server.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,42 @@
 const express = require('express');
 const app = express();
+const ObjectID = require('mongodb').ObjectId;
 
 app.use(express.json());
 const MongoClient = require('mongodb').MongoClient;
-const createRouter = require('./helpers/create_router.js');
 
 MongoClient.connect('mongodb://localhost:27017')
     .then((client) => {
 
-        const db = client.db('stocks');
-        const companiesCollection = db.collection('companies');
-        const companiesRouter = createRouter(companiesCollection);
-   
-        app.use('/api/stocks', companiesRouter);
+        const db = client.db('players');
+        const playerInfo = db.collection('playerInfo');
+        app.get('/api/players', (req, res) => {
+            playerInfo
+                .find()
+                .toArray()
+                .then((docs) => res.json(docs))
+                .catch((err) => {
+                    console.error(err);
+                    res.status(500);
+                    res.json({ status: 500, error: err });
+                });
+        })
+
+        app.put('/api/players/:id', (req, res) => {
+            const id = req.params.id;
+            const updatedData = req.body;
+            delete updatedData._id;
+            playerInfo
+                .updateOne({ _id: ObjectID(id) }, { $set: updatedData })
+                .then(result => {
+                    res.json(result);
+                })
+                .catch((err) => {
+                    res.status(500);
+                    res.json({ status: 500, error: err });
+                });
+        });
+
     })
         app.listen(5000, function() {
             console.log(`Stock server running on port ${this.address().port}`);
